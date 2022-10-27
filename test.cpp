171e62a09ad133e9012e4838ebda24f09e0dd1a7
Maksim Bidenko <m.bidenko@bk.ru>
2022-10-28 01:56:54 +0300

    Fix googletest, ready for run

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 #include "googletest/googletest/include/gtest/gtest.h"
+#include "googletest/googletest/include/gtest/gtest-assertion-result.h"
 #include "Parallelepiped.hpp"
 #include "PriorityQueue.hpp"
 
 TEST(Group1, test1) {
-    PriorityQueue<double>
-    ASSERT_DOUBLE_EQ();
+    PriorityQueue<double> pq;
+    ASSERT_DOUBLE_EQ(1., 1.);
 }
 
 int main(int argc, char **argv)
