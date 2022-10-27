623bb49292f2ab8d7f99c35772ed13e19dd5c121
Maksim Bidenko <m.bidenko@bk.ru>
2022-10-27 23:33:13 +0300

    Refactoring hierarchy

--- PriorityQueue.hpp
+++ PriorityQueue.hpp
@@ -4,12 +4,12 @@
 #include <vector>
 #include <algorithm>
 
-
 template<typename T>
 class PriorityQueue : public std::vector<T> {
 private:
     void sive_up(size_t start) {
         while (this[start] > this[start / 2]) {
+            //std::swap(this[start], this[start / 2]);
             T tmp = this->at(start);
             this->at(start) = this->at(start / 2);
             this->at(start / 2) = tmp;
@@ -23,18 +23,16 @@ public:
 
 template<typename T>
 void PriorityQueue<T>::push(const T& item) {
-    std::cout << "push" << std::endl;
     std::cout << this->size() << std::endl;
     this->push_back(item);
-    this->sive_up(this->size());
+    this->sive_up(this->size() - 1);
 }
 
 template<typename T>
 T PriorityQueue<T>::pop() {
-    std::cout << "pop" << std::endl;
     T tmp = this->front();
     std::swap(this->front(), this->back());
     this->pop_back();
-    this->sive_up(this->size());
+    this->sive_up(this->size() - 1);
     return tmp;
 }
